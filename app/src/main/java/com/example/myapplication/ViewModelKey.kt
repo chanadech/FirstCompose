a981ede6bfc6ba7228a42710781bef1e9937428d
chanadech <dach.boy@>
2024-03-18 23:13:43 +0700

    Try to run (must update- didnt show)

---
+++ app/src/main/java/com/example/myapplication/ViewModelKey.kt
@@ -0,0 +1,9 @@
+package com.example.myapplication
+import androidx.lifecycle.ViewModel
+import dagger.MapKey
+import kotlin.reflect.KClass
+
+@Target(AnnotationTarget.FUNCTION)
+@Retention(AnnotationRetention.RUNTIME)
+@MapKey
+annotation class ViewModelKey(val value: KClass<out ViewModel>)
